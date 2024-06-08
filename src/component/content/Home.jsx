c47e63e1a36058cff4a96dbb6878cbc39ad44733
桜小路七葉 <Nana7ha@outlook.com>
2024-06-08 15:36:27 +0800

    feat: 架构搭建

---
+++ src/component/content/Home.jsx
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+import Base from './Base';
+
+
+class Home extends Component {
+    state = {}
+
+    render() {
+        return (<Base>
+                首页
+            </Base>);
+    }
+}
+
+export default Home;
